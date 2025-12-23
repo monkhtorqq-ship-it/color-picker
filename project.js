5c495450f2758be0564c3b3ad395bf964198e4e5
PADA-1-5-04 <PADA-1-5-04@Erxess-iMac.local>
2025-12-23 16:41:06 +0800

    tsasnii ungu soligddog bollo

--- project.js
+++ project.js
@@ -1,42 +1,62 @@
-const colorPicker = document.getElementById("colorPicker");
-const hexInput = document.getElementById("hexInput");
-const preview = document.getElementById("preview");
-const rgbValue = document.getElementById("rgbValue");
-const snow = document.getElementsByClassName("snow");
+document.addEventListener('DOMContentLoaded', () => {
+
+  const colorPicker = document.getElementById("colorPicker");
+  const hexInput = document.getElementById("hexInput");
+  const preview = document.getElementById("preview");
+  const rgbValue = document.getElementById("rgbValue");
+  const snow = document.getElementsByClassName("snow");
+
   /* hex to rgb */
-function hexToRgb(hex) {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {
+  function hexToRgb(hex) {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? {
         r: parseInt(result[1], 16),
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16),
       }
-    : null;
-}
-  /*  */
-function updateColor(color) {
-  preview.style.backgroundColor = color;
-  hexInput.value = color;
-  snow.style.backgroundColor = color;
-  const rgb = hexToRgb(color);
-  if (rgb) {
-    rgbValue.textContent = `RGB: ${rgb.r}, ${rgb.g}, ${rgb.b}`;
-  }
-}
-
-colorPicker.addEventListener("input", (e) => {
-  updateColor(e.target.value);
-});
-
-hexInput.addEventListener("input", (e) => {
-  const hex = e.target.value;
-  if (/^#[0-9A-F]{6}$/i.test(hex)) {
-    colorPicker.value = hex;
-    updateColor(hex);
+      : null;
   }
 
+  /* ongoo huwilgah */
+  function updateColor(color) {
+    preview.style.backgroundColor = color;
+    hexInput.value = color;     /* ene mur ajilahgui baina shu */
+    snow.style.backgroundColor = color;
+    const rgb = hexToRgb(color);
+    if (rgb) {
+      rgbValue.textContent = `RGB: ${rgb.r}, ${rgb.g}, ${rgb.b}`;
+    }
+  }
+
+  /* ongiig ni ajiglah */
+  colorPicker.addEventListener("input", (e) => {
+    updateColor(e.target.value);
+  });
+  hexInput.addEventListener("input", (e) => {
+    let hex = e.target.value;
+    if (/^#[0-9A-F]{6}$/i.test(hex)) {
+      colorPicker.value = hex;
+      updateColor(hex);
+    }
   });
 
-updateColor(colorPicker.value);
-  
+  /* hex/rgb huulj awah */
+  preview.addEventListener("click", () => {
+    let hex = hexInput.value;
+    if (hex) {
+      navigator.clipboard.writeText(hex)
+        .then(() => alert(`Coppied hex: ${hex}`))
+        .catch(err => alert("Failed to copy" + err))
+    }
+  });
+  rgbValue.addEventListener("click", () => {
+    let rgb = hexToRgb(hexInput.value);
+    if (rgb) {
+      navigator.clipboard.writeText(`${rgb.r}, ${rgb.g}, ${rgb.b}`)
+        .then(() => alert(`${rgb.r}, ${rgb.g}, ${rgb.b}`))
+        .catch(err => alert("Failed to copy" + err))
+    };
+  })
+
+})
--- tsas.js
+++ tsas.js
@@ -1,20 +1,58 @@
-const total = 200;
+document.addEventListener('DOMContentLoaded', () => {
 
-function randomRange(min, max) {
-  return min + Math.floor(Math.random() * (max - min + 1));
-}
+  const total = 200;
+  function randomRange(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
+  }
+  
+  for (let i = 0; i < total; i++) {
+    const snowflake = document.createElement("div");
+    snowflake.className = "snow";
+    
+    const x = Math.random() * 100;
+    const scale = Math.random();
+    const duration = randomRange(10, 30);
+    
+    snowflake.style.left = `${x}vw`;
+    snowflake.style.animationDuration = `${duration}s`;
+    snowflake.style.setProperty("--scale", scale);
+    
+    document.body.appendChild(snowflake);
+  }
 
-for (let i = 0; i < total; i++) {
-  const snowflake = document.createElement("div");
-  snowflake.className = "snow";
+  /* ongiig ni ajiglah */
+  colorPicker.addEventListener("input", (e) => {
+    disco(e.target.value);
+  });
+  hexInput.addEventListener("input", (e) => {
+    let hex = e.target.value;
+    if (/^#[0-9A-F]{6}$/i.test(hex)) {
+      colorPicker.value = hex;
+      disco(hex);
+    }
+  });
 
-  const x = Math.random() * 100;
-  const scale = Math.random();
-  const duration = randomRange(10, 30);
+  tsasniiarray = Array.from(tsasnuud);
 
-  snowflake.style.left = `${x}vw`;
-  snowflake.style.animationDuration = `${duration}s`;
-  snowflake.style.setProperty("--scale", scale);
+  /* ongiig ni oorchloh */
+  function disco(color) {
+    const tsasnuud = document.querySelectorAll(".snow");
+    tsasnuud.forEach(snow => {
+      snow.style.backgroundColor = color;
+    })
+  }
+})
 
-  document.body.appendChild(snowflake);
-}
+/* ⠀⠀⠀⠀⠀⢀⣤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⢤⣤⣀⣀⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡼⠋⠀⣀⠄⡂⠍⣀⣒⣒⠂⠀⠬⠤⠤⠬⠍⠉⠝⠲⣄⡀⠀⠀
+⠀⠀⠀⢀⡾⠁⠀⠊⢔⠕⠈⣀⣀⡀⠈⠆⠀⠀⠀⡍⠁⠀⠁⢂⠀⠈⣷⠀⠀
+⠀⠀⣠⣾⠥⠀⠀⣠⢠⣞⣿⣿⣿⣉⠳⣄⠀⠀⣀⣤⣶⣶⣶⡄⠀⠀⣘⢦⡀     i have no idea how any of this works
+⢀⡞⡍⣠⠞⢋⡛⠶⠤⣤⠴⠚⠀⠈⠙⠁⠀⠀⢹⡏⠁⠀⣀⣠⠤⢤⡕⠱⣷
+⠘⡇⠇⣯⠤⢾⡙⠲⢤⣀⡀⠤⠀⢲⡖⣂⣀⠀⠀⢙⣶⣄⠈⠉⣸⡄⠠⣠⡿
+⠀⠹⣜⡪⠀⠈⢷⣦⣬⣏⠉⠛⠲⣮⣧⣁⣀⣀⠶⠞⢁⣀⣨⢶⢿⣧⠉⡼⠁
+⠀⠀⠈⢷⡀⠀⠀⠳⣌⡟⠻⠷⣶⣧⣀⣀⣹⣉⣉⣿⣉⣉⣇⣼⣾⣿⠀⡇⠀
+⠀⠀⠀⠈⢳⡄⠀⠀⠘⠳⣄⡀⡼⠈⠉⠛⡿⠿⠿⡿⠿⣿⢿⣿⣿⡇⠀⡇⠀
+⠀⠀⠀⠀⠀⠙⢦⣕⠠⣒⠌⡙⠓⠶⠤⣤⣧⣀⣸⣇⣴⣧⠾⠾⠋⠀⠀⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠙⠶⣭⣒⠩⠖⢠⣤⠄⠀⠀⠀⠀⠀⠠⠔⠁⡰⠀⣧⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠲⢤⣀⣀⠉⠉⠀⠀⠀⠀⠀⠁⠀⣠⠏⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠒⠲⠶⠤⠴⠒⠚⠁      */
